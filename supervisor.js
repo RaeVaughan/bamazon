b6d8cc6c0465d69bb1bd43470002911772cda88b
RaeVaughan <rachelev.tx@gmail.com>
2017-07-09 18:15:56 -0500

    supervisor view product sales by department functionality; table working correctly

--- supervisor.js
+++ supervisor.js
@@ -5,7 +5,7 @@ var colors = require("colors");
 
 var table = new Table({
 		head: ["Dpt ID", "Department", "Overhead Costs", "Product Sales", "Total Profit"],
-		colWidths: [5, 25, 15, 8, 5]
+		colWidths: [6, 20, 18, 17, 14]
 	});
 table.options.style.head = ["green"];
 
@@ -44,9 +44,22 @@ function supervisorPrompt(){
 	});
 }
 
-//need to write a function to create the inner join table and log it
 function viewProductSales(){
-	console.log("Under contstruction");
+
+	connection.query("SELECT departments.department_id, departments.department_name, departments.overhead_costs, products.product_sales FROM products INNER JOIN departments ON (products.department_name = departments.department_name) GROUP BY department_name ORDER BY departments.department_id;", function(err, res){
+
+		if (err) throw err; 
+
+		for (var i = 0; i < res.length; i++) {
+			var totalProfit =  res[i].product_sales - res[i].overhead_costs;
+
+			table.push(
+	    	[res[i].department_id, res[i].department_name, res[i].overhead_costs, res[i].product_sales, totalProfit]
+			);
+		}
+		console.log(table.toString());
+	});
+
 	// connection.query("SELECT * FROM products", function(err, res) {
 	// 	for (var i = 0; i < res.length; i++) {
 	// 		table.push(
@@ -58,7 +71,63 @@ function viewProductSales(){
 }
 
 function createDpt(){
-	console.log("create new department working");
+
 }
 
+// function addNewProduct(){
+// 	connection.query("SELECT * FROM products", function(err, res){
+// 		if (err) throw err;
+// 		inquirer.prompt([
+// 			{
+// 				type: "input",
+// 				name: "product",
+// 				message: "What product would you like to add?"
+// 			},
+// 			{
+// 				type: "input",
+// 				name: "department",
+// 				message: "What department is this product in?"
+// 			},
+// 			{
+// 				type: "input",
+// 				name: "price",
+// 				message: "What is the price of this product?",
+// 				validate: function(value) {
+// 					if (isNaN(value) === false && parseInt(value) > 0) {
+// 						return true;
+// 					}
+// 					return false;
+// 				}
+// 			},
+// 			{
+// 				type: "input",
+// 				name: "stock",
+// 				message: "What is the starting inventory of this product?",
+// 				validate: function(value) {
+// 					if (isNaN(value) === false && parseInt(value) > 0) {
+// 						return true;
+// 					}
+// 					return false;
+// 				}
+// 			}
+// 		]).then(function(answers) {
+// 			var newProduct = answers.product;
+// 			var newProductDpt = answers.department;
+// 			var newProductPrice = answers.price;
+// 			var stock = answers.stock;
+
+// 			connection.query("INSERT INTO products SET ?",
+// 				{
+// 					product_name: newProduct,
+// 					department_name: newProductDpt,
+// 					price: newProductPrice,
+// 					stock_quantity: stock
+// 				}, function(err, res) {
+// 					console.log(stock + " of the new product (" + newProduct + ") added to the " + newProductDpt + " department for $" + newProductPrice);
+// 					continuePrompt();
+// 				}
+// 			);
+// 		});
+// 	});
+// }
 
